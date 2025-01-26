8d22baee3f8af72d091a203e1f7df5b92d8a89f8
muxazzXJohny <maracoco334@gmail.com>
2025-01-26 20:16:27 +0200

    Add files via upload

---
+++ ConsoleApp17-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
