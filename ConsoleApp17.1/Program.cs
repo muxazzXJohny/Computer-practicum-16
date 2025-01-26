b768b52586d7b1ea8c6c5968ac643cb04cac4b8c
muxazzXJohny <maracoco334@gmail.com>
2025-01-26 20:17:13 +0200

    task-2

---
+++ ConsoleApp17.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j <= 15; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
